b3ae056614e06d06dfb81df99ab9c078fdc7514e
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-10 15:10:24 +0200

    Stub some methods in NaiveBayes

--- src/org/panhandlers/sentimentalizer/NaiveBayes.java
+++ src/org/panhandlers/sentimentalizer/NaiveBayes.java
@@ -1,16 +1,25 @@
 package org.panhandlers.sentimentalizer;
 
 public class NaiveBayes implements Classifier {
-
+	private ClassifierStorage storage;
+	
+	public NaiveBayes() {
+		storage = new HashStorage();
+	}
+	
 	@Override
 	public void train(String category, String text) {
-		// TODO Auto-generated method stub
-		
+		// TODO: need to split strings and maybe stemming and stuff like that...
+		storage.addFeature(category, text);
 	}
 
 	@Override
 	public ClassificationResult classify(String text) {
 		return null;
+	}
+	
+	private float pOfFeatureGivenCategory(String feature, String category) {
+		return 0.0f;
 	}
 
 }

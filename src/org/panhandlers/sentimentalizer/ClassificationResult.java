0402395babd31ab4e93ffcb1af80240df760bf9b
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-11 08:50:39 +0200

    Add compareTo, equals and hashCode methods

--- src/org/panhandlers/sentimentalizer/ClassificationResult.java
+++ src/org/panhandlers/sentimentalizer/ClassificationResult.java
@@ -1,6 +1,6 @@
 package org.panhandlers.sentimentalizer;
 
-public class ClassificationResult {
+public class ClassificationResult implements Comparable<ClassificationResult> {
 	private float p;
 	private String category;
 	
@@ -20,6 +20,47 @@ public class ClassificationResult {
 	}
 	public void setCategory(String category) {
 		this.category = category;
+	}
+	@Override
+	public int compareTo(ClassificationResult o) {
+		if (this.equals(o)) {
+			return 0;
+		} 
+		if(o.getP() > this.getP()) {
+			return -1;
+		}
+		if(o.getP() < this.getP()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((category == null) ? 0 : category.hashCode());
+		result = prime * result + Float.floatToIntBits(p);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClassificationResult other = (ClassificationResult) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (Float.floatToIntBits(p) != Float.floatToIntBits(other.p))
+			return false;
+		return true;
 	}
 	
 	

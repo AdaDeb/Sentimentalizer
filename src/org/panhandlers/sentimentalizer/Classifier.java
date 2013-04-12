18298fcf22a91f993c7f1e2c2a4e6c41469eb88f
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-12 12:57:26 +0200

    Classifier should take list of Features

--- src/org/panhandlers/sentimentalizer/Classifier.java
+++ src/org/panhandlers/sentimentalizer/Classifier.java
@@ -1,6 +1,13 @@
 package org.panhandlers.sentimentalizer;
 
+import java.util.List;
+
 public interface Classifier {
-	public void train(String category, String text);
-	public ClassificationResult classify(String text);
+	/**
+	 * Train the Classifier for one item. 
+	 * @param category
+	 * @param features A list of features that make up one item.
+	 */
+	public void train(String category, List<Feature> features);
+	public ClassificationResult classify(List<Feature> features);
 }

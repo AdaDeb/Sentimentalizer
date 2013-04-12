3239cb12de6b79af8181729dd9518db0e19c879a
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-12 11:34:37 +0200

    Save some memory, perhaps

--- src/org/panhandlers/sentimentalizer/OccurrenceFeatureExtractor.java
+++ src/org/panhandlers/sentimentalizer/OccurrenceFeatureExtractor.java
@@ -8,13 +8,14 @@ import java.util.Set;
 public class OccurrenceFeatureExtractor implements FeatureExtractor {
 	private Set<String> dictionary;
 	private HashMap<String, OccurrenceFeature> tokensToFeatures;
+	private ArrayList<Feature> features;
 	public OccurrenceFeatureExtractor (Set<String> dictionary) {
 		this.dictionary = dictionary;
 	}
 	@Override
 	public List<Feature> extractFeatures(ArrayList<String> input) {
 		tokensToFeatures = new HashMap<String, OccurrenceFeature>();
-		ArrayList<Feature> features = new ArrayList<Feature>();
+		features = new ArrayList<Feature>();
 		// Initialize with 0 occurrences for each token in dictionary
 		OccurrenceFeature feature;
 		for (String token : dictionary) {

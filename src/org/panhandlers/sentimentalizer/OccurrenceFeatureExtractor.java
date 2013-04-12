82c346c952ecbc203db5a2089c4d08305e4ab688
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-12 11:31:58 +0200

    Implement OccurrenceFeatureExtractor

--- src/org/panhandlers/sentimentalizer/OccurrenceFeatureExtractor.java
+++ src/org/panhandlers/sentimentalizer/OccurrenceFeatureExtractor.java
@@ -1,11 +1,33 @@
 package org.panhandlers.sentimentalizer;
 
-public class OccurrenceFeatureExtractor implements FeatureExtractor {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
+public class OccurrenceFeatureExtractor implements FeatureExtractor {
+	private Set<String> dictionary;
+	private HashMap<String, OccurrenceFeature> tokensToFeatures;
+	public OccurrenceFeatureExtractor (Set<String> dictionary) {
+		this.dictionary = dictionary;
+	}
 	@Override
-	public Feature[] extractFeatures(String input) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Feature> extractFeatures(ArrayList<String> input) {
+		tokensToFeatures = new HashMap<String, OccurrenceFeature>();
+		ArrayList<Feature> features = new ArrayList<Feature>();
+		// Initialize with 0 occurrences for each token in dictionary
+		OccurrenceFeature feature;
+		for (String token : dictionary) {
+			feature = new OccurrenceFeature();
+			features.add(feature);
+			tokensToFeatures.put(token, new OccurrenceFeature());
+		}
+		for (String token : input) {
+			if (tokensToFeatures.containsKey(token)) {
+				tokensToFeatures.get(token).increment();
+			}
+		}
+		return features;
 	}
 
 }

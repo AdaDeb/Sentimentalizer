5622216eaffc6a8f94df8ac7af7fe2504b113ac8
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-13 10:30:20 +0200

    Add stemming to tokenizer

--- src/org/panhandlers/sentimentalizer/LuceneTokenizer.java
+++ src/org/panhandlers/sentimentalizer/LuceneTokenizer.java
@@ -9,6 +9,7 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.util.Version;
+import org.tartarus.snowball.ext.EnglishStemmer;
 
 
 /**
@@ -22,20 +23,26 @@ import org.apache.lucene.util.Version;
 public class LuceneTokenizer implements GeneralTokenizer{
 	private Analyzer analyzer;
 	private ArrayList<String> output;
+	private EnglishStemmer stemmer;
 	
 	public LuceneTokenizer() {
-		analyzer = new StandardAnalyzer(Version.LUCENE_42, StandardAnalyzer.STOP_WORDS_SET);// new SimpleAnalyzer();
+		stemmer = new EnglishStemmer();
+		analyzer = new StandardAnalyzer(Version.LUCENE_42, StandardAnalyzer.STOP_WORDS_SET);
 	}
 	
 	@Override
 	public ArrayList<String> tokenize(String input) {
 		output = new ArrayList<String>();
+		String s;
 		try {
 			TokenStream ts = analyzer.tokenStream("wut", new StringReader(input));
 			CharTermAttribute attr = ts.addAttribute(CharTermAttribute.class);
 			ts.reset();
 			while (ts.incrementToken()) {
-				output.add(attr.toString());
+				stemmer.setCurrent(attr.toString());
+				stemmer.stem();
+				s = stemmer.getCurrent();
+				output.add(s);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -50,7 +57,7 @@ public class LuceneTokenizer implements GeneralTokenizer{
 	 */
 	public static void main(String[] args) {
 		GeneralTokenizer t = new LuceneTokenizer();
-		ArrayList<String> strings = t.tokenize("Hello and goodbye and mète a tête the bat is my bat.");
+		ArrayList<String> strings = t.tokenize("Hello and transitive hugenot goodbye and mète a tête the bat is my bat.");
 		for (String s : strings) {
 			System.out.println(s);
 		}

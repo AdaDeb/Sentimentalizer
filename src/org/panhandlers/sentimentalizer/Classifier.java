4a5c17c2c280bd0141c021c3b7d263629a535adb
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-18 13:16:22 +0200

    Add multipleTrain method

--- src/org/panhandlers/sentimentalizer/Classifier.java
+++ src/org/panhandlers/sentimentalizer/Classifier.java
@@ -1,6 +1,8 @@
 package org.panhandlers.sentimentalizer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public interface Classifier {
 	/**
@@ -9,5 +11,16 @@ public interface Classifier {
 	 * @param features A list of features that make up one item.
 	 */
 	public void train(String category, List<Feature> features);
+	
+	/**
+	 * Classify an item, represented by a list of features.
+	 * @param features
+	 * @return
+	 */
 	public ClassificationResult classify(List<Feature> features);
+	
+	/**
+	 * Train the classifier on multiple items
+	 */
+	public void multipleTrain(HashMap<String, List<List<Feature>>> trainingSet, Set<String> dictionary);
 }

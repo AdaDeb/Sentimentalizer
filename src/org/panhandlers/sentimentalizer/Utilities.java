de38de69f23d52670e3dc6f6fb75b65c62445f70
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-05-01 14:24:47 +0200

    Add a detokenizer utility

--- src/org/panhandlers/sentimentalizer/Utilities.java
+++ src/org/panhandlers/sentimentalizer/Utilities.java
@@ -1,10 +1,16 @@
 package org.panhandlers.sentimentalizer;
 
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
 public class Utilities {
+	private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
+	private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
 	public static <T> List<Pair<T>> combinations(List<T> list) {
 		List<Pair<T>> pairs = new LinkedList<Pair<T>>();
 		Pair<T> pair;
@@ -31,5 +37,20 @@ public class Utilities {
 		}
 		Double standardDeviation = Math.sqrt(sumOfSquares / successRates.size());
 		return standardDeviation;
+	}
+	
+	public static String normalize(String input) {
+	    String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
+	    String normalized = Normalizer.normalize(nowhitespace, Form.NFD);
+	    String slug = NONLATIN.matcher(normalized).replaceAll("");
+	    return slug.toLowerCase(Locale.ENGLISH);
+	}
+	
+	public static String detokenize(List<String> input) {
+		StringBuilder b = new StringBuilder();
+		for (String s : input) {
+			b.append(s + " ");
+		}
+		return b.toString();
 	}
 }
